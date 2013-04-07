7ff34e66c998351f7c5e4f6483b7a163de37bb56
bcbroom <brian.broom@gmail.com>
2013-04-07 19:18:18 -0400

    Initial Commit

---
+++ TowerDefenseUpdate/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  TowerDefenseUpdate
+//
+//  Created by Brian Broom on 4/7/13.
+//  Copyright Brian Broom 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TowerDefenseUpdate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TowerDefenseUpdate
+//
+//  Created by Brian Broom on 4/7/13.
+//  Copyright Brian Broom 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
